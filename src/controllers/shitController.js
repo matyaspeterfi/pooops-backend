f5f9900d977296d612e0606c7db4aa385772cf61
rekatoth <reka.rebeka.toth@gmail.com>
2020-01-24 14:38:20 +0100

    feat(collect shit): PUT /shit endpoint

--- src/controllers/shitController.js
+++ src/controllers/shitController.js
@@ -3,19 +3,26 @@ class ShitController {
     this.shitService = shitService;
     this.getShits = this.getShits.bind(this);
     this.postShit = this.postShit.bind(this);
+    this.putShit = this.putShit.bind(this);
     this.getIdFromToken = getIdFromToken;
   }
 
   getShits(req, res) {
     this.shitService.getAllShits()
       .then(data => res.status(200).json(data))
-      .catch(err => res.status(err.message).json(`You made a ${err.message} error`))
+      .catch(err => res.status(err.message).json(`You made a ${err.message} error`));
   }
 
   postShit(req, res) {
     this.shitService.postNewShit(req.body.shit, this.getIdFromToken(req))
       .then(data => res.status(200).json(data))
-      .catch(err => res.status(err.message).json(`You made a ${err.message} error`))
+      .catch(err => res.status(err.message).json(`You made a ${err.message} error`));
+  }
+
+  putShit(req, res) {
+    this.shitService.removeShit(req.headers.shitid, this.getIdFromToken(req))
+      .then(data => res.status(200).json(data))
+      .catch(err => res.status(err.message).json(`You made a ${err.message} error`));
   }
 }
 
