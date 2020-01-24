6f30603420f345fc958601185e3e2185e3d90d8a
Daniel Kristaly <kristaly.daniel92@gmail.com>
2020-01-24 23:40:12 +0100

    leaderboard is done

--- src/controllers/shitController.js
+++ src/controllers/shitController.js
@@ -4,6 +4,7 @@ class ShitController {
     this.getShits = this.getShits.bind(this);
     this.postShit = this.postShit.bind(this);
     this.putShit = this.putShit.bind(this);
+    this.getOldestShits = this.getOldestShits.bind(this);
     this.getIdFromToken = getIdFromToken;
   }
 
@@ -21,6 +22,12 @@ class ShitController {
 
   putShit(req, res) {
     this.shitService.removeShit(req.headers.shitid, this.getIdFromToken(req))
+      .then(data => res.status(200).json(data))
+      .catch(err => res.status(err.message).json(`You made a ${err.message} error`));
+  }
+
+  getOldestShits(req, res) {
+    this.shitService.getOldestShits()
       .then(data => res.status(200).json(data))
       .catch(err => res.status(err.message).json(`You made a ${err.message} error`));
   }
