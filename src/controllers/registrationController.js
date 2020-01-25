16070a7ad3ce00005426b16be9be34496482208c
Mat Peterfi <matyas.peterfi@gmail.com>
2020-01-25 05:12:00 +0100

    registration confirmation email

--- src/controllers/registrationController.js
+++ src/controllers/registrationController.js
@@ -1,12 +1,15 @@
 class RegistrationController {
-  constructor(registrationService) {
+  constructor(registrationService, mailService) {
     this.registrationService = registrationService;
+    this.mailService = mailService;
     this.register = this.register.bind(this);
   }
 
   register(req, res) {
     this.registrationService.createUser(req.body)
-      .then(data => res.status(200).json(data))
+      .then(data => {
+        this.mailService.main(req.body.email);
+        res.status(200).json(data)})
       .catch(err => res.status(err.message).json(`You made a ${err.message} error`));
   }
 }
